200b43a7915ea3ad096b30e65a203e1dbc61bcc4
zhoushume <873625226@qq.com>
2018-04-05 19:38:53 +0800

    t

--- test_case/test_login.py
+++ test_case/test_login.py
@@ -1,14 +1,18 @@
 # coding:utf-8
 import requests
 import unittest
+import  json
 
 class get_request(unittest.TestCase):
     def setUp(self):
-        self.get_url = 'https://www.bmqb.com/'
+        self.get_url = 'https://test-m.bmqb.com/api/about/v2/event.json'
+
     def test_post_01(self):
         url=self.get_url
         r = requests.get(url)
-        print (r.text)
+        print(json.dumps(r.json(), indent=4))
+        assert r.status_code == 200
+
     def tearDown(self):
         pass
 if __name__ == "__main__":
